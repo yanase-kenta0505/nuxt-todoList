36f84134676417e243c8d86ddc6786d2208028b1
yanase-kenta0505 <ynsknt0505@gmail.com>
2021-10-16 06:34:01 +0900

    add : todoにスライド機能を追加

--- pages/test.vue
+++ pages/test.vue
@@ -1,50 +1,69 @@
 <template>
-  <v-card>
-    <v-toolbar color="cyan" dark flat>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-app>
+    <div class="mx-auto">
+      <button @click="add">追加</button>
 
-      <v-toolbar-title>Your Dashboard</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-
-      <template v-slot:extension>
-        <v-tabs v-model="tab" align-with-title>
-          <v-tabs-slider color="yellow"></v-tabs-slider>
-
-          <v-tab v-for="item in items" :key="item">
-            {{ item }}
-          </v-tab>
-        </v-tabs>
-      </template>
-    </v-toolbar>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item v-for="item in items" :key="item">
-        <v-card flat>
-          <v-card-text v-text="text"></v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
+      <ul>
+        <transition-group name="fade">
+          <li
+            style="cursor: pointer"
+            v-for="(number, index) in numbers"
+            :key="number"
+            @click="remove(index)"
+          >
+            {{ number }}
+          </li>
+        </transition-group>
+      </ul>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      tab: null,
-      items: ["web", "shopping", "videos", "images", "news"],
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      numbers: [0, 1, 2],
+      nextNumber: 3,
+      show: true
     };
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    }
   }
 };
 </script>
+
+<style scoped>
+.fade-enter {
+  opacity: 0;
+  transform: translateX(-150px);
+}
+.fade-enter-active {
+  transition: transform 3s, opacity 3s;
+}
+.fade-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+.fade-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+.fade-leave-active {
+  transition: transform 3s, opacity 3s;
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(150px);
+}
+</style>
